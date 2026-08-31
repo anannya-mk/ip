ca5fe036a7e0dc587f60872029e7b3f35b937a4f
anann <e1591082@u.nus.edu>
2026-08-31 21:13:57 +0800

    Echo, Add, List, Mark as done

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -1,6 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Represents Nova, a command-line task list assistant with a sarcastic,
+ * theatrical personality. Nova tracks a list of tasks that can be added,
+ * marked, unmarked, and listed via simple text commands entered on the
+ * command line.
+ */
+
+
 public class Nova {
-    public static void main(String[] args) {
-        String banner =  "\n"
+
+    private static final String MARKED_PREFIX = "[X] ";
+    private static final String UNMARKED_PREFIX = "[ ] ";
+
+    /**
+     * Prints Nova's ASCII art logo to the console in cyan.
+     */
+    public static void printLogo() {
+        String cyan = "\u001B[36m";
+        String reset = "\u001B[0m";
+
+        String logo = "\n"
                 + "ooooo      ooo  .oooooo.  oooooo     oooo      .o.      \n"
                 + "`888b.     `8' d8P'  `Y8b  `888.     .8'      .888.     \n"
                 + " 8 `88b.    8 888      888  `888.   .8'      .8\"888.    \n"
@@ -8,6 +31,125 @@ public class Nova {
                 + " 8     `88b.8 888      888    `888.8'      .88ooo8888   \n"
                 + " 8       `888 `88b    d88'     `888'      .8'     `888. \n"
                 + "o8o        `8  `Y8bood8P'       `8'      o88o     o8888o\n";
-        System.out.println(banner);
+
+        System.out.println(cyan + logo + reset);
+    }
+
+    /**
+     * Prints a randomly chosen, sarcastic success message to the console.
+     */
+    public static void success() {
+        String[] phrases = {"Flawless, as expected of me.",
+                "Success. Try to contain your amazement.",
+                "There. Was that so hard?",
+                "Another triumph. You're welcome.",
+                "Perfectly executed, naturally."};
+        Random rand = new Random();
+        int idx = rand.nextInt(phrases.length);
+
+        System.out.println("\t" + phrases[idx]);
+    }
+
+    /**
+     * Prints a randomly chosen greeting to the console when Nova starts.
+     */
+    public static void greet() {
+        String[] greetings = {"Ah, look who seeks my counsel once again. How... predictable. What is it this time?",
+                "You return. I confess, I did wonder if you'd have the nerve.",
+                "Well, well. Look who requires my particular brand of brilliance. "
+                        + "Tell me then, what are we doing today?",
+                "Ah, you've returned... how disappointing. Very well, what do you want?",
+                "Oh great one, how may I assist you on this dreadful occasion?",
+                "....And here I'd hoped I wouldn't see you for a time yet. "
+                        + "Alright, let's keep this snappy- what do you want?"};
+
+        Random rand = new Random();
+        int idx = rand.nextInt(greetings.length);
+
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\t" + greetings[idx]);
+        System.out.println("\t____________________________________________________________\n");
+    }
+
+    /**
+     * Prints a randomly chosen farewell message to the console when Nova exits.
+     */
+    public static void farewell() {
+        String[] farewells = {"Leaving so soon? I suppose even amateurs need their rest.",
+                "Farewell, then. Try not to miss me too terribly.",
+                "Ah, fleeing already? Coward. Go on, then.",
+                "Until next time. Do try to bring a better question with you.",
+                "Leaving? Very well. I have far grander things to attend to anyway.",
+                "Goodbye. I'll be here, plotting, as always."};
+
+        Random rand = new Random();
+        int idx = rand.nextInt(farewells.length);
+
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\t" + farewells[idx]);
+        System.out.println("\t____________________________________________________________\n");
+    }
+
+    /**
+     * Runs Nova's command loop, reading commands from standard input until
+     * the user enters "bye".
+     *
+     * @param args Command-line arguments (unused).
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("____________________________________________________________");
+        printLogo();
+        greet();
+
+        List<String> entryList = new ArrayList<>();
+        List<String> status = new ArrayList<>();
+
+        while (true) {
+            String line = scanner.nextLine();
+            if (line.equalsIgnoreCase("bye")) {
+                break;
+            } else if (line.equalsIgnoreCase("list")) {
+                if (entryList.isEmpty()) {
+                    System.out.println("\tEmpty. Truly groundbreaking work you've done here. "
+                            + "You need to add something in before "
+                            + "I can list it out for you, genius.");
+                } else {
+                    for (int i = 0; i < entryList.size(); i++) {
+                        System.out.println("\t" + (i + 1) + "." + status.get(i) + entryList.get(i));
+                    }
+                    System.out.println();
+                    success();
+                }
+            } else if (line.toLowerCase().startsWith("unmark")) {
+                String[] parts = line.split(" ");
+                int idx = Integer.parseInt(parts[1]) - 1;
+                if (status.get(idx).equals(MARKED_PREFIX)) {
+                    status.set(idx, UNMARKED_PREFIX);
+                    System.out.println("\tYet another.. distraction.... how fortunate I am. "
+                            + "Very well, your task is unmarked.");
+                    System.out.println("\t" + (idx + 1) + "." + status.get(idx) + entryList.get(idx));
+                } else {
+                    System.out.println("\tWell, isn't that embarrassing. This was already unmarked, you moron.");
+                }
+            } else if (line.toLowerCase().startsWith("mark")) {
+                String[] parts = line.split(" ");
+                int idx = Integer.parseInt(parts[1]) - 1;
+                if (status.get(idx).equals(UNMARKED_PREFIX)) {
+                    status.set(idx, MARKED_PREFIX);
+                    System.out.println("\tHuh. I'm almost proud. Almost.\n\tYour task has been marked.");
+                    System.out.println("\t" + (idx + 1) + "." + status.get(idx) + entryList.get(idx));
+                } else {
+                    System.out.println("\tOh, you simpleton, this was already marked.");
+                }
+            } else {
+                System.out.println("\t____________________________________________________________");
+                System.out.println("\tadded: " + line);
+                status.add(UNMARKED_PREFIX);
+                entryList.add(line);
+                System.out.println("\t____________________________________________________________");
+            }
+        }
+        farewell();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,54 @@
+/**
+ * Represents a single task with a description and a completion status.
+ */
+public class Task {
+
+    protected String description;
+    protected boolean isDone;
+
+    /**
+     * Creates a new, incomplete task with the given description.
+     *
+     * @param description Description of the task.
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Returns the status icon representing whether this task is done.
+     *
+     * @return "X" if the task is done, otherwise a blank space.
+     */
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    /**
+     * Marks this task as done.
+     */
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    /**
+     * Marks this task as not done.
+     */
+    public void markAsNotDone() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}
