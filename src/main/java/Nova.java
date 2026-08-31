0249ca1908a68753ddac615d567e15998ef5695a
anann <e1591082@u.nus.edu>
2026-08-31 21:05:59 +0800

    Name change

---
+++ src/main/java/Nova.java
@@ -0,0 +1,13 @@
+public class Nova {
+    public static void main(String[] args) {
+        String banner =  "\n"
+                + "ooooo      ooo  .oooooo.  oooooo     oooo      .o.      \n"
+                + "`888b.     `8' d8P'  `Y8b  `888.     .8'      .888.     \n"
+                + " 8 `88b.    8 888      888  `888.   .8'      .8\"888.    \n"
+                + " 8   `88b.  8 888      888   `888. .8'      .8' `888.   \n"
+                + " 8     `88b.8 888      888    `888.8'      .88ooo8888   \n"
+                + " 8       `888 `88b    d88'     `888'      .8'     `888. \n"
+                + "o8o        `8  `Y8bood8P'       `8'      o88o     o8888o\n";
+        System.out.println(banner);
+    }
+}
